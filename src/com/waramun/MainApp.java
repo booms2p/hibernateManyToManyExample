6994f9b519870521032598dd442ac97c518841ca
Waramun Achametra <waramuna@corp.ais900.org>
2018-01-18 13:50:51 +0700

    Update delete method

--- src/com/waramun/MainApp.java
+++ src/com/waramun/MainApp.java
@@ -2,8 +2,12 @@ package com.waramun;
 
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
+
 import com.waramun.FootballPlayer;
 import com.waramun.EplClub;
 import com.waramun.HibernateUtil;
@@ -39,6 +43,25 @@ public class MainApp {
 //		FootballPlayer player = (FootballPlayer)session.get(FootballPlayer.class, 28);
 //		EplClub club = (EplClub)session.get(EplClub.class, 3);
 //		player.getEplClub().remove(club);
+//		===========================
+		
+//		String stm = "FROM FootballPlayer FP WHERE FP.playerLname = 'ह'";
+//		Integer a = 0; 
+//		Query query = session.createQuery(stm);
+//		List res = query.list();
+//		for (Iterator iterator = res.iterator(); iterator.hasNext();){
+//			FootballPlayer playerRes = (FootballPlayer) iterator.next();
+//			System.out.print(playerRes.getPlayerFname());
+//			System.out.print(" ");
+//			System.out.print(playerRes.getPlayerLname());
+//			System.out.println("");
+//			a = playerRes.getPlayerId();
+//		}
+//		
+//		System.out.println(a);
+//		FootballPlayer player = (FootballPlayer)session.get(FootballPlayer.class, a);
+//		session.delete(player);
+//		
 //		===========================
 		
 		session.getTransaction().commit();
